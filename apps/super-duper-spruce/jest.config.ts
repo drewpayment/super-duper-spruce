ac9240bdf04bac80286cbf6cd47d72f00b7581c9
drewpayment <drew.payment@gmail.com>
2022-06-11 14:49:03 -0400

    initial commit

---
+++ apps/super-duper-spruce/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'super-duper-spruce',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/super-duper-spruce',
+};
