4cff63c31b8982ca46ed9f6029f4d7cc299f7bb2
drewpayment <drew.payment@gmail.com>
2022-06-13 16:29:54 -0400

    update readme.md and small fixes
    - updated readme
    - added screenshots
    - updated filter button label

--- apps/super-duper-spruce/pages/properties/index.tsx
+++ apps/super-duper-spruce/pages/properties/index.tsx
@@ -253,7 +253,7 @@ const PropertiesPage: NextPage = (ctx: any) => {
 
           <div className="flex justify-end mt-6">
             <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-              Save
+              Filter
             </button>
           </div>
         </form>
